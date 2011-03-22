3d776a3fd1da3047c7bb02ac5883206b7032eb26
Devan Goodwin <dgoodwin@redhat.com>
2011-03-22 11:15:07 -0300

    Initialize bare Compliance Manager project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComplianceManager::Application.config.secret_token = 'c03fec9aaf4c4aef3c74a18e60ac8e9f65107271314fa8230b9e51da20e825fac01eef96bdcde434f2e4fe09101a02ab72bc9665e7cc0ca494440a3b626eaaf8'

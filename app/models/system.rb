960c023502e418346e3e16c9b6ac74fd778e1a29
Devan Goodwin <dgoodwin@redhat.com>
2011-04-06 12:42:21 -0300

    Remove unecessary new check in system model.

--- app/models/system.rb
+++ app/models/system.rb
@@ -10,13 +10,5 @@ class System < Base
   # Candlepin API expects an owner key as the ID:
   self.primary_key = :uuid
 
-  # ActiveResource assumes anything with an ID is a pre-existing
-  # resource, ID in our case is key, and key is manually assigned at creation,
-  # so we must override the new check to force active record to persist our
-  # new org.
-  def new?
-    return (not (@attributes.has_key?(:id) and not @attributes[:id].nil?))
-  end
-
 end
 

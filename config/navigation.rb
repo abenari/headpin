408586406b9a24be87eaec6dc5bcfb353d2443ba
Devan Goodwin <dgoodwin@redhat.com>
2011-04-11 15:46:49 -0300

    Make sub-items in menus conditional.

--- config/navigation.rb
+++ config/navigation.rb
@@ -7,15 +7,36 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |top_level|
 
     top_level.item :dashboard, _("Dashboard"), {:controller => 'dashboard'}
-    top_level.item :organizations, _("Organizations"), {:controller => 'organizations'}, :class=>'organizations' do |orgs_sub|
-      orgs_sub.item :subscriptions, _("Subscriptions"), (@organization.nil? || @organization.id.nil?) ? "" : subscriptions_organization_path(@organization.id)
-      orgs_sub.item :systems, _("Systems"), (@organization.nil? || @organization.id.nil?) ? "" : systems_organization_path(@organization.key)
+    
+    top_level.item :organizations, _("Organizations"), 
+      {:controller => 'organizations'}, :class=>'organizations' do |orgs_sub|
+
+      orgs_sub.item :subscriptions, _("Subscriptions"), 
+        (@organization.nil? || @organization.id.nil?) ? "" : 
+          subscriptions_organization_path(@organization.id),
+          :if => Proc.new { not @organization.nil? }
+      
+      orgs_sub.item :systems, _("Systems"), 
+        (@organization.nil? || @organization.id.nil?) ? "" : 
+          systems_organization_path(@organization.key),
+          :if => Proc.new { not @organization.nil? }
+
     end 
 
-    top_level.item :systems, _("Systems"), {:controller => 'systems'}, :class=>'systems' do |systems_sub|
-      systems_sub.item :show, _("Details"), @system.nil? ? "" : system_path(@system.uuid)
-      systems_sub.item :subscriptions, _("Current Subscriptions"), @system.nil? ? "" : subscriptions_system_path(@system.uuid)
-      systems_sub.item :subscriptions, _("Available Subscriptions"), @system.nil? ? "" : "/systems/#{@system.uuid}/available_subscriptions"
+    top_level.item :systems, _("Systems"), {:controller => 'systems'}, 
+      :class=>'systems' do |systems_sub|
+
+      systems_sub.item :show, _("Details"), @system, 
+        :if => Proc.new { not @system.nil? }
+
+      systems_sub.item :subscriptions, _("Current Subscriptions"), 
+        @system.nil? ? "" : subscriptions_system_path(@system.uuid), 
+        :if => Proc.new { not @system.nil? }
+      
+      systems_sub.item :subscriptions, _("Available Subscriptions"), 
+        @system.nil? ? "" : "/systems/#{@system.uuid}/available_subscriptions", 
+        :if => Proc.new { not @system.nil? }
+
     end
     
     

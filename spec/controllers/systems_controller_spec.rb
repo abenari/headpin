2bf099dde7e67661997a2bdb6338c1479de83472
Devan Goodwin <dgoodwin@redhat.com>
2011-04-12 13:09:13 -0300

    Add a couple system controller tests.

---
+++ spec/controllers/systems_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe SystemsController do
+
+  include LoginHelperMethods
+  include MockHelperMethods
+
+  before (:each) do
+    login_user
+  end
+
+  describe "GET index" do
+
+    it 'should be successful' do
+      org = mock_org()
+      Organization.should_receive(:find).with(org.key).and_return(org)
+      System.should_receive(:find).with(:all, {:params => {:owner => org.key}}).
+        and_return([mock_system])
+
+      # Get index, simulate session setting for the current organization:
+      get 'index', {}, {:current_organization_id => org.key}
+      response.should be_success
+    end
+
+    context 'with no working org selected' do
+      it 'should redirect to org selection' do
+      # Get index, no current org in the session:
+      get 'index'
+      response.should redirect_to("http://test.host/admin/organizations")
+      end
+    end
+
+  end
+
+
+end
+
--- spec/helpers/mock_helper_methods.rb
+++ spec/helpers/mock_helper_methods.rb
@@ -3,7 +3,12 @@ module MockHelperMethods
   def mock_org key=nil, displayName=nil
     key ||= random_string
     displayName ||= random_string
+    # TODO: this is sufficient for an org as far as we're concerned, but an
+    # actual org queried from Candlepin will have many other properties:
     mock(Organization, :key => key, :displayName => displayName)
+  end
+
+  def mock_system
   end
 
   def random_string(prefix=nil)
